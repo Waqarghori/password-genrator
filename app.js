0e67340d6ef1f85c9d42956ef85ce05caa7ade81
Waqarghori <waqar.iqbal789@gmail.com>
2025-03-16 18:20:47 +0500

    upd

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+
+
 let jzUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let jzNumbers = "0123456789";
 let jzSpecial = "!@#$%^&*()_+{}[]|:;<>,.?/~`";
@@ -11,12 +13,12 @@ let numbers = document.getElementById("numbers");
 let special = document.getElementById("special-char");
 let showPass = document.getElementById("result");
 
-let password = "abcdefghijklmnopqrstuvwxyz"; 
+let password = "abcdefghijklmnopqrstuvwxyz";
 
 let getnrate = document.getElementById("generate")
-getnrate.addEventListener("click",function(){
-    
-    if(inputLen.value == "" || inputLen.value <= 1 || inputLen.value > 50){
+getnrate.addEventListener("click", function () {
+
+    if (inputLen.value == "" || inputLen.value <= 1 || inputLen.value > 50) {
         alert("Please enter a valid password length")
     }
     if (upper.checked) {
@@ -30,9 +32,17 @@ getnrate.addEventListener("click",function(){
     }
     let result = "";
     for (let i = 0; i < inputLen.value; i++) {
-        let random = Math.floor(Math.random()* password.length)
+        let random = Math.floor(Math.random() * password.length)
         result += password.charAt(random)
-        
+
     }
     showPass.innerHTML = result;
+    inputLen.value = ""
 })
+
+let clear = document.getElementById("clear");
+clear.addEventListener("click",function clear() {
+    showPass.innerHTML = "";
+})
+
+
